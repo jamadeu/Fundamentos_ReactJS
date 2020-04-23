8a2f68d4cea21e175c36a82f90a82f03321e522c
jamadeu <jean.amadeu2@gmail.com>
2020-04-23 12:19:00 -0300

    End

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,12 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number | Date): string => {
+  if (typeof value === 'number') {
+    return Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(value);
+  }
 
+  const date = new Date(value);
+  return Intl.DateTimeFormat('pt-BR').format(date);
+};
 export default formatValue;
